397e0ea54c8b518b2f61171339f4c9292d87d84d
Alain NGUIDJOI BELL <alain.nguidjoi.bell@gmail.com>
2018-02-11 19:34:23 +0400

    Init first commit

---
+++ src/main/java/com/devware/backend/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.devware.backend.config.audit;
---
+++ src/main/java/com/devware/backend/web/rest/errors/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Specific errors used with Zalando's "problem-spring-web" library.
+ *
+ * More information on https://github.com/zalando/problem-spring-web
+ */
+package com.devware.backend.web.rest.errors;
